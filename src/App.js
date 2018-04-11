6912c775245fb05be3f0debcd4e002a7d6daf6b1
Jenni <silverrain64@gmail.com>
2018-04-10 20:49:34 -0700

    Separating front- and back-ends for development

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './database'
 
 class App extends Component {
   render() {
--- src/database.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-var connect = mongoose.connect('mongodb://localhost:27017/acme', {
-	useMongoClient: true
-});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
