337afe07f1db3c53e54dae58b254f09056932326
Jenni <silverrain64@gmail.com>
2018-05-30 18:36:50 -0700

    Planning components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+"use strict;"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <h1><span className="title-logo">ACME</span> Website Company</h1>
+          <h1><span className="title-logo">ACME</span> Device Company</h1>
           <nav>
             <ul>
               <li><a>About Us</a></li>
@@ -18,14 +19,13 @@ class App extends Component {
         </header>
         <main>
           <div className="about">
-            <h2>The #1 source for all your mayhem needs</h2>
             <div>
               <p>
                 For over 65 years, ACME Company has... Oh, how I wish I could believe or understand that! There's only one reasonable course of action now: kill Flexo! Meh. We're also Santa Claus! That's a popular name today. Little "e", big "B"?
-      And remember, don't do anything that affects anything, unless it turns out you were supposed to, in which case, for the love of God, don't not do it! Oh dear! She's stuck in an infinite loop, and he's an idiot! Well, that's love for you.
-      Why am I sticky and naked? Did I miss something fun? Hey! I'm a porno-dealing monster, what do I care what you think? What's with you kids? Every other day it's food, food, food. Alright, I'll get you some stupid food.
-      We can't compete with Mom! Her company is big and evil! Ours is small and neutral! Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor.
-            </p>
+                And remember, don't do anything that affects anything, unless it turns out you were supposed to, in which case, for the love of God, don't not do it! Oh dear! She's stuck in an infinite loop, and he's an idiot! Well, that's love for you.
+                Why am I sticky and naked? Did I miss something fun? Hey! I'm a porno-dealing monster, what do I care what you think? What's with you kids? Every other day it's food, food, food. Alright, I'll get you some stupid food.
+                We can't compete with Mom! Her company is big and evil! Ours is small and neutral! Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor.
+              </p>
               <img src="/img/whatnowending.jpg" alt="Wile E. Coyote with sign saying 'What do I do now?'" />
             </div>
           </div>
