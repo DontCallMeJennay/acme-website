5c9f5d3247e0a5c3ff900bf8c2ea19d146c364e1
Jenni <silverrain64@gmail.com>
2018-05-30 20:23:23 -0700

    React-Router works, but now the styling is broken...?

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-//import "../css/about.css";
+import React from 'react';
+import "../css/about.css";
 
-export default class AboutUs extends React.Component {
+export default class About extends React.Component {
     render() {
         return (
             <div>
@@ -11,7 +11,7 @@ export default class AboutUs extends React.Component {
                     Why am I sticky and naked? Did I miss something fun? Hey! I'm a porno-dealing monster, what do I care what you think? What's with you kids? Every other day it's food, food, food. Alright, I'll get you some stupid food.
                     We can't compete with Mom! Her company is big and evil! Ours is small and neutral! Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor.
               </p>
-                <img src="/img/whatnowending.jpg" alt="Wile E. Coyote with sign saying 'What do I do now?'" />
+                <img src="/img/whatnowpic.jpg" alt="Wile E. Coyote with sign saying 'What do I do now?'" />
             </div>
         )
     }
