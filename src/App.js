06aa79d8504aaa1acfb663981e30673aa3e9a88a
Jenni <silverrain64@gmail.com>
2018-05-30 17:33:32 -0700

    Scaffolding...

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ class App extends Component {
       <div className="App">
         <header>
           <h1><span className="title-logo">ACME</span> Website Company</h1>
+          <nav>
+            <ul>
+              <li><a>About Us</a></li>
+              <li><a>Products</a></li>
+              <li><a>Contact</a></li>
+              <li><a></a></li>
+            </ul>
+          </nav>
         </header>
         <main>
-
+          <div id="home">
+            <img src="public/img/whatnowending.jpg" alt="Wile E. Coyote with sign saying 'What do I do now?'" />
+            <p>
+              Oh, how I wish I could believe or understand that! There's only one reasonable course of action now: kill Flexo! Meh. We're also Santa Claus! That's a popular name today. Little "e", big "B"?
+    And remember, don't do anything that affects anything, unless it turns out you were supposed to, in which case, for the love of God, don't not do it! Oh dear! She's stuck in an infinite loop, and he's an idiot! Well, that's love for you.
+    Why am I sticky and naked? Did I miss something fun? Hey! I'm a porno-dealing monster, what do I care what you think? What's with you kids? Every other day it's food, food, food. Alright, I'll get you some stupid food.
+    We can't compete with Mom! Her company is big and evil! Ours is small and neutral! Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor. Humans dating robots is sick. You people wonder why I'm still single? It's 'cause all the fine robot sisters are dating humans!
+    Throw her in the brig. And yet you haven't said what I told you to say! How can any of us trust you? Fry, you can't just sit here in the dark listening to classical music. THE BIG BRAIN AM WINNING AGAIN! I AM THE GREETEST! NOW I AM LEAVING EARTH, FOR NO RAISEN!
+    I guess because my parents keep telling me to be more ladylike. As though! I found what I need. And it's not friends, it's things. And from now on you're all named Bender Jr. Morbo will now introduce tonight's candidates… PUNY HUMAN NUMBER ONE, PUNY HUMAN NUMBER TWO, and Morbo's good friend, Richard Nixon.
+            </p>
+          </div>
         </main>
         <footer>
-
         </footer>
-
       </div>
     );
   }
